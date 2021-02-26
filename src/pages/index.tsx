ea63961bf25942a9b098361cce78fb3c59fe73dd
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-02-26 20:24:40 -0300

    aula04

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css';
 import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 export default function Home() {
   return (
@@ -17,16 +18,18 @@ export default function Home() {
 
       <ExperienceBar />
 
-      <section>
-        <div>
+      <CountdownProvider>
+        <section>
+          <div>
             <Profile />
             <CompletedChallenges />
             <Countdown />
-        </div>
-        <div>
+          </div>
+          <div>
             <ChallengeBox />
-        </div>
-      </section>
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
