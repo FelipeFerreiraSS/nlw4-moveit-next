538954589d50e931569e72f668cb58f21e62ad56
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-02-28 16:38:12 -0300

    update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -11,7 +11,7 @@ export function Profile() {
             <div>
                 <strong>Felipe Ferreira</strong>
                 <p>
-                    <img src="icons/Level.svg" alt="level"/>
+                    <img src="icons/level.svg" alt="level"/>
                     Level {level}
 
                 </p>
